88a68c8e2287ce98bfa2e01b2f1bd7c378b4e412
QmarSys <q54919966@gmail.com>
2026-01-05 12:55:22 +0000

    fix: improve priority word detection in CardBody component

--- src/components/card/cardBody.vue
+++ src/components/card/cardBody.vue
@@ -3,9 +3,9 @@ const props = defineProps<{ text: string }>()
 let colorClass = 'bg-gray-200 text-gray-800'
 const textArr = props.text.split(' ')
 const proiotyWord = textArr[textArr.length - 1]
-if (proiotyWord === '!!!') {
+if (proiotyWord?.includes('!!!')) {
   colorClass = 'bg-red-200 text-red-800'
-} else if (proiotyWord === '!!') {
+} else if (proiotyWord?.includes('!!')) {
   colorClass = 'bg-yellow-200 text-yellow-800'
 } else {
   colorClass = 'bg-green-200 text-green-800'

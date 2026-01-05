3ed49764826d150bc5ba2c2b3a37077cc8c3ba24
QmarSys <q54919966@gmail.com>
2026-01-05 12:50:58 +0000

    fix: correct priority word detection and update CardBody text format in FormBody

--- src/components/card/cardBody.vue
+++ src/components/card/cardBody.vue
@@ -2,10 +2,10 @@
 const props = defineProps<{ text: string }>()
 let colorClass = 'bg-gray-200 text-gray-800'
 const textArr = props.text.split(' ')
-const proiotyWord = textArr[0]
-if (proiotyWord === '!!!:') {
+const proiotyWord = textArr[textArr.length - 1]
+if (proiotyWord === '!!!') {
   colorClass = 'bg-red-200 text-red-800'
-} else if (proiotyWord === '!!:') {
+} else if (proiotyWord === '!!') {
   colorClass = 'bg-yellow-200 text-yellow-800'
 } else {
   colorClass = 'bg-green-200 text-green-800'

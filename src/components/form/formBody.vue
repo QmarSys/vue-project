22317c729905515dec0cbc76b874d69653be1a9f
QmarSys <q54919966@gmail.com>
2026-01-05 12:52:05 +0000

    fix: update guidance text for item priority in FormBody component

--- src/components/form/formBody.vue
+++ src/components/form/formBody.vue
@@ -9,7 +9,7 @@ const { list } = defineProps<{ list: string[] }>()
       <CardBody v-for="(item, index) in list" :key="index" :text="`${index + 1}. ${item}`" />
     </div>
     <div class="text-gray-400 h-full w-full flex justify-center items-center" v-else>
-      add !!!: for very important , !!: for important , else for normal items
+      add !!! for very important , !! for important , else for normal items at the end of the item
     </div>
   </body>
 </template>
